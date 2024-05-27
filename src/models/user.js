8dacc6b4a4d5d42a117943401bac4ba19653b901
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-05-27 14:30:44 +0530

    create user model

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+// initiate schema
+const userSchema = new mongoose.Schema({
+    full_name: {
+        type: String,
+        required: true
+      },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'deleted'],
+        default: 'active'
+    }
+});
+
+// from schema to model
+const User = mongoose.model('User', userSchema);
+module.exports = User; 
