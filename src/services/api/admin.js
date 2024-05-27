89b5657aa316c7dda091dffdef9c8fec6dc2b45e
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-05-27 17:16:17 +0530

    Update to return token for auth after login

--- src/services/api/admin.js
+++ src/services/api/admin.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const bcrypt = require('bcrypt');
 const adminModel = require('../../models/admin');
+const jwt = require('jsonwebtoken');
 
 async function adminLogin(usernameOrEmail, password) {
-
     // check account
     const adminFound = await adminModel.findOne({ 
         $or: [{ email: usernameOrEmail }, { username: usernameOrEmail }]
@@ -13,7 +14,27 @@ async function adminLogin(usernameOrEmail, password) {
     const isMatch = await bcrypt.compare(password, adminFound.password);
     if (!isMatch) throw { status: 401, message: 'Wrong password' };
 
-    return adminFound;
+    // initiate payload for token
+    const payload = {
+        email: adminFound.email,
+        username: adminFound.username,
+        full_name: adminFound.full_name,
+    };
+
+    // Generaste token and data admin
+    const accessToken = jwt.sign( payload, process.env.SECRET_KEY_ACCESS, { expiresIn: '30m' });
+    const refreshToken = jwt.sign( payload, process.env.SECRET_KEY_REFRESH, { expiresIn: '8h' });
+    const admin = adminFound;
+
+    // remove field status and password
+    delete admin.status;
+    delete admin.password;
+
+    return {
+        accessToken,
+        refreshToken,
+        admin
+    };
 }
 
 module.exports = {
