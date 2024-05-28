5a491c847817ee077deddf98c66a6419a4c5342a
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-05-28 16:20:01 +0530

    Create filter status for customer

--- src/services/api/customer.js
+++ src/services/api/customer.js
@@ -7,6 +7,9 @@ async function getDataCustomers(filter) {
     let queryFilter = {};
     if (filter && Object.keys(filter).length > 0) {
 
+        // handle filter status
+        filter['status'] = filter.status ?? 'active';
+
         // handle filter full name
         if (filter.full_name) {
             filter.full_name = { $regex: filter.full_name, $options: 'i' };
@@ -57,9 +60,9 @@ async function getDataCustomers(filter) {
         // delete unecessary filter for query to mongoose
         delete filter.date_range;
         delete filter.balance_range;
-        queryFilter = filter;
     };
-    
+
+    queryFilter = filter;
     return await customerModel.find(queryFilter).lean();
 };
 
