f43542d2690b6cddcece7443525b2135fa6b57dd
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-05-29 15:22:33 +0530

    Create end point deposit

--- src/routes/main.js
+++ src/routes/main.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const adminRoutes = require('./admin');
 const customerRoutes = require('./customer');
 const wasteTypeRoutes = require('./wasteType');
+const depositRoutes = require('./deposit');
 
 router.use('/admin', adminRoutes);
 router.use('/customer', customerRoutes);
 router.use('/wasteType', wasteTypeRoutes);
+router.use('/deposit', depositRoutes);
 
 module.exports = router;
