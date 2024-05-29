3d15bac8913e54c39033151edbbd92722c24f74e
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-05-29 11:28:00 +0530

    create model wastetype and fix some letter case

---
+++ src/models/wastetype.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+// initiate schema
+const wasteTypeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        default: [0, 'Amount must be at least 0']
+    },
+    status: {
+        type: String,
+        enum: ['active', 'deleted'],
+        default: 'active'
+    },
+}, { timestamps: true });
+
+// from schema to model
+const wasteType = mongoose.model('wasteType', wasteTypeSchema);
+module.exports = wasteType; 
--- src/routes/wastetype.js
+++ src/routes/wastetype.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const wastetypeController = require('../controllers/wastetype');
+const wasteTypeController = require('../controllers/wasteType');
 const adminController = require('../controllers/admin');
 
 // initiate router
 const router = express.Router();
 
 // Sub route / for wastetype
-router.get('/', adminController.auth, wastetypeController.getWastetype);
-router.post('/', adminController.auth, wastetypeController.createWastetype);
-router.put('/', adminController.auth, wastetypeController.updateWastetype);
-router.delete('/', adminController.auth, wastetypeController.deleteWastetype);
+router.get('/', adminController.auth, wasteTypeController.getWasteType);
+router.post('/', adminController.auth, wasteTypeController.createWasteType);
+router.put('/', adminController.auth, wasteTypeController.updateWasteType);
+router.delete('/', adminController.auth, wasteTypeController.deleteWasteType);
 
 module.exports = router;
